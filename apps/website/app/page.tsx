07d20dc4e44ab2dcea559d8b4138d3057919bcb1
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-10-18 09:40:55 +0500

    added hover effects to elements on root page

--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -1,9 +1,19 @@
-import { Logo } from "@/components/layout/logo"
+import { Logo } from "@/components/logo"
 import { Separator } from "@yz13/mono/components/separator"
-import { LockIcon, UserCircleIcon } from "lucide-react"
+import { ContactIcon, LayoutGridIcon, LockIcon, MapIcon, UserCircleIcon } from "lucide-react"
+import { cn } from "yz13/cn"
 
 const page = () => {
-  const list = Array.from({ length: 10 }).map((_, i) => i)
+  const list = [
+    {
+      id: "ee22-vfqwe",
+      title: "Pzzlr (Puzzler)",
+      href: null,
+      status: "plan",
+      created_at: "2023-01-01T00:00:00.000Z",
+      icon: null
+    }
+  ]
   const Highlight = ({ children }: { children: React.ReactNode }) => (
     <span className="text-foreground hover:underline transition-colors font-bold font-pixel uppercase">{children}</span>
   )
@@ -25,22 +35,36 @@ const page = () => {
           <div className="flex items-start gap-1 flex-wrap">
             <span className="inline-flex p-0.5 pr-1.5 border rounded-full items-center gap-1">
               <span className="size-4 rounded-full border" />
-              <span className="text-xs text-secondary">Location</span>
+              <span className="text-xs text-secondary">Russia</span>
             </span>
           </div>
           <div className="w-full grid h-16 grid-cols-4 gap-2">
-            <button className="w-full h-full rounded-xl border flex flex-col items-center justify-center">
+            <button
+              className="w-full h-full rounded-xl border flex flex-col items-center justify-center hover:bg-yz-neutral-100 hover:border-foreground"
+            >
               <UserCircleIcon className="text-secondary" size={16} />
               <span className="text-secondary text-xs">Profile</span>
             </button>
-            <button className="w-full h-full rounded-xl border flex flex-col items-center justify-center">
+            <button
+              className="w-full h-full rounded-xl border flex flex-col items-center justify-center hover:bg-yz-neutral-100 hover:border-foreground"
+            >
+              <ContactIcon className="text-secondary" size={16} />
+              <span className="text-secondary text-xs">Contacts</span>
             </button>
-            <button className="w-full h-full rounded-xl border flex flex-col items-center justify-center">
+            <button
+              className="w-full h-full rounded-xl border flex flex-col items-center justify-center hover:bg-yz-neutral-100 hover:border-foreground"
+            >
+              <MapIcon className="text-secondary" size={16} />
+              <span className="text-secondary text-xs">Roadmap</span>
             </button>
-            <button className="w-full h-full rounded-xl border flex flex-col items-center justify-center">
+            <button
+              className="w-full h-full rounded-xl border flex flex-col items-center justify-center hover:bg-yz-neutral-100 hover:border-foreground"
+            >
+              <LayoutGridIcon className="text-secondary" size={16} />
+              <span className="text-secondary text-xs">Services</span>
             </button>
           </div>
-        </div>
+        </div >
         <Logo />
         <p className="text-secondary">
           Hey, i'm a fulstack developer, i love to build things.
@@ -56,43 +80,67 @@ const page = () => {
               list
                 .filter((_, i) => i <= 4)
                 .map(item => {
+
+                  const status = item.status
+                  const statuses = ["plan", "dev", "approval", "prod"]
+                  const statusIndex = statuses.indexOf(status)
                   return (
                     <li
                       key={`pr-${item}`}
-                      className="w-full hover:bg-yz-neutral-100 peer rounded-xl border"
+                      className={cn(
+                        "w-full peer rounded-xl border transition-colors",
+                        "hover:border-foreground hover:bg-yz-neutral-100"
+                      )}
                     >
                       <div className="p-2.5 h-fit flex gap-3 flex-col">
-                        <div className="flex items-center gap-2">
-                          <div className="size-7 border rounded-full" />
-                          <div className="flex flex-col">
-                            <h3 className="text-base">Project#{item}</h3>
+                        <div className="flex flex-col gap-1.5">
+                          <div className="flex items-center gap-2">
+                            <div className="size-5 border rounded-full" />
+                            <div className="flex flex-col">
+                              <h3 className="text-sm line-clamp-2">{item.title}</h3>
+                            </div>
                           </div>
+                          {
+                            item.href &&
+                            <span className="inline-flex hover:underline text-xs hover:text-foreground text-secondary items-center gap-1.5">
+                              <LockIcon size={12} className="text-inherit" />
+                              https://example.dev
+                            </span>
+                          }
                         </div>
-                        <span className="inline-flex hover:underline text-xs hover:text-foreground text-secondary items-center gap-1.5">
-                          <LockIcon size={12} className="text-inherit" />
-                          https://example.dev
-                        </span>
                         <div className="flex w-full items-center justify-between">
                           <span
-                            className="text-xs shrink-0 px-2 py-0.5 rounded-md border text-secondary"
+                            className={cn(
+                              "text-xs shrink-0 px-2 py-0.5 rounded-md border text-secondary",
+                              statusIndex === 0 ? "border-foreground text-foreground" : ""
+                            )}
                           >
                             Plan
                           </span>
                           <Separator className="sm:!w-4 w-6 inline-block" />
                           <span
-                            className="text-xs shrink-0 px-2 py-0.5 rounded-md border text-secondary"
+                            className={cn(
+                              "text-xs shrink-0 px-2 py-0.5 rounded-md border text-secondary",
+                              statusIndex === 1 ? "border-foreground text-foreground" : ""
+                            )}
                           >
                             Dev
                           </span>
                           <Separator className="sm:!w-4 w-6 inline-block" />
                           <span
-                            className="text-xs shrink-0 px-2 py-0.5 rounded-md border text-secondary"
+                            className={cn(
+                              "text-xs shrink-0 px-2 py-0.5 rounded-md border text-secondary",
+                              statusIndex === 2 ? "border-foreground text-foreground" : ""
+                            )}
                           >
                             Approval
                           </span>
                           <Separator className="sm:!w-4 w-6 inline-block" />
                           <span
-                            className="text-xs shrink-0 px-2 py-0.5 rounded-md border text-secondary"
+                            className={cn(
+                              "text-xs shrink-0 px-2 py-0.5 rounded-md border text-secondary",
+                              statusIndex === 3 ? "border-foreground text-foreground" : ""
+                            )}
                           >
                             Prod
                           </span>
@@ -114,7 +162,7 @@ const page = () => {
             </div>
           </div>
         </footer>
-      </div>
+      </div >
     </>
   )
 }
