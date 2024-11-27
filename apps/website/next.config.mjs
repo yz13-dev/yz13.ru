96352beb412f8875f2398a70556d194a91b55521
YZ13-ENV <YZTHECEO@yandex.ru>
2024-11-27 20:40:17 +0500

    fix next.config

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: [ "mono", "yz13" ],
-  experimental: {
-    serverComponentsExternalPackages: [ "mono", "yz13" ],
-  },
   images: {
     remotePatterns: [
       {
