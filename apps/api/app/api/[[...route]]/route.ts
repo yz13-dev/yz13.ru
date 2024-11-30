f7c5d97da752d5fe5892b078ea1684291fdbcf77
YZ13-ENV <YZTHECEO@yandex.ru>
2024-11-30 19:46:36 +0500

    make rewrite from /api/* to /*, for api app

--- apps/api/app/api/[[...route]]/route.ts
+++ apps/api/app/api/[[...route]]/route.ts
@@ -2,13 +2,23 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import { user } from "./user";
 import { user_workspace } from "./workspace";
+import packageJson from "@/package.json"
 
 export const runtime = "edge";
 
-const app = new Hono().basePath("/api");
+const app = new Hono().basePath("/");
 
 app.route("/user/:uid/workspace", user_workspace);
 app.route("/user", user);
+
+app.get("/version", (c) => {
+  const version = packageJson.version
+  return c.json(version);
+});
+
+app.get("/health", (c) => {
+  return c.json({ status: "ok" });
+});
 
 export const GET = handle(app);
 export const POST = handle(app);
--- apps/api/next.config.mjs
+++ apps/api/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/api/:path*",
+      }
+    ]
+  }
+};
 
 export default nextConfig;
