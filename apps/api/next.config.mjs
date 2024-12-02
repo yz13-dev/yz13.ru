4250f635b14bd21934e636be1096704d21809aa4
YZ13-ENV <YZTHECEO@yandex.ru>
2024-12-02 18:35:26 +0500

    remove workpace app, try to make build work on vercel

--- apps/api/next.config.mjs
+++ apps/api/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: [ "mono", "yz13" ],
   async rewrites() {
     return [
       {
--- apps/workspace/actions/work/schema.ts
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-
-export default z.object({
-  id: z.string(),
-})
