511a7d8c05fe0719f73da9c32158a3d5978d0039
YZ13-ENV <YZTHECEO@yandex.ru>
2025-01-09 16:08:35 +0500

    update root page, update middleware

--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -1,7 +1,30 @@
-import { permanentRedirect } from "next/navigation";
+import { Logo } from "@/components/logo";
+import Availability from "./(root)/(public)/discover/availability";
 
 const page = () => {
-  return permanentRedirect("/discover");
+  return (
+    <div className="w-full h-dvh flex flex-col items-center justify-center gap-6">
+      <div className="w-full max-w-md space-y-6">
+        <div className="flex items-start gap-3">
+          <div className="size-14 shrink-0 rounded-full bg-yz-neutral-200 flex items-center justify-center">
+            <Logo className="size-8" />
+          </div>
+          <div className="flex flex-col gap-1">
+            <span className="text-foreground text-xl font-medium inline-flex items-start flex-wrap gap-2">
+              YZ13,
+              <span className="text-secondary">
+                frontend developer, nothing crazy.
+              </span>
+            </span>
+            <span className="text-secondary">On my way to fullstack.</span>
+          </div>
+        </div>
+        <div className="w-full px-4">
+          <Availability />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
