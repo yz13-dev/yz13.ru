b4b3e12b36fe5212470dc398a8d14c2140668121
YZ13-ENV <YZTHECEO@yandex.ru>
2025-02-01 20:25:40 +0500

    update maps page

--- apps/website/app/maps/overlay.tsx
+++ apps/website/app/maps/overlay.tsx
@@ -1,20 +1,16 @@
 "use client";
-import {
-  ArrowLeftIcon,
-  ChevronLeftIcon,
-  EllipsisIcon,
-  NavigationIcon,
-  RulerIcon,
-} from "lucide-react";
+import { BookmarkIcon } from "lucide-react";
 import { Button } from "mono/components/button";
 import { Input } from "mono/components/input";
-import Link from "next/link";
+import { Separator } from "mono/components/separator";
+import { motion } from "motion/react";
 import { useRouter } from "next/navigation";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { setLat, setLng } from "./map.store";
 
 const Overlay = ({ children }: { children?: ReactNode }) => {
   const router = useRouter();
+  const [open, setOpen] = useState<boolean>(false);
   const getUserCoords = () => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -32,46 +28,36 @@ const Overlay = ({ children }: { children?: ReactNode }) => {
   return (
     <>
       {children}
-      <div className="absolute left-0 top-0 w-fit p-4">
-        <div className="flex flex-row items-center gap-1 p-1 rounded-lg bg-background relative">
-          <Button size="icon" className="shrink-0" variant="ghost" asChild>
-            <Link href="/">
-              <ArrowLeftIcon size={18} />
-            </Link>
-          </Button>
-          <Input placeholder="Поиск" className="w-64" />
-          <Button
-            size="icon"
-            className="shrink-0 px-1 w-fit absolute -right-8"
-            variant="secondary"
-          >
-            <ChevronLeftIcon size={18} />
-          </Button>
-        </div>
-      </div>
       <div className="absolute right-0 top-0 w-fit p-4 flex items-center gap-2">
-        <div className="flex flex-row items-center gap-1 p-1 rounded-lg bg-background">
-          <Button
-            size="icon"
-            onClick={getUserCoords}
-            className="shrink-0"
-            variant="ghost"
-          >
-            <NavigationIcon size={18} />
-          </Button>
-        </div>
-        <div className="flex flex-row items-center gap-1 p-1 rounded-lg bg-background">
-          <Button size="icon" className="shrink-0" variant="ghost">
-            <RulerIcon size={18} />
-          </Button>
-          <Button size="icon" className="shrink-0" variant="ghost">
-            <EllipsisIcon size={18} />
-          </Button>
-        </div>
         <div className="flex flex-row items-center gap-1 p-1 rounded-lg bg-background">
           <div className="size-9 rounded-full border" />
         </div>
       </div>
+      <motion.div
+        onClick={() => setOpen(!open)}
+        initial={{ width: "24rem", bottom: "-5rem" }}
+        whileHover={{ width: "36rem", bottom: "0" }}
+        animate={
+          open
+            ? { width: "36rem", bottom: "0" }
+            : { width: "24rem", bottom: "-5rem" }
+        }
+        layoutId="overlay-box"
+        className="absolute left-0 right-0 gap-4 flex max-w-full flex-col h-fit mx-auto p-4 rounded-t-3xl items-center bg-background z-20"
+      >
+        <div className="flex w-full flex-row items-center gap-2">
+          <Input placeholder="Поиск" className="w-full" />
+          <Button variant="secondary" size="icon" className="shrink-0">
+            <BookmarkIcon size={18} />
+          </Button>
+        </div>
+        <Separator />
+        <div className="flex flex-row items-center gap-2">
+          <button className="size-12 rounded-xl border"></button>
+          <button className="size-12 rounded-xl border"></button>
+          <button className="size-12 rounded-xl border"></button>
+        </div>
+      </motion.div>
     </>
   );
 };
