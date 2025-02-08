cad6eed84e5a9d1a69007909a897fcb1c020decb
YZ13-ENV <YZTHECEO@yandex.ru>
2025-02-08 21:18:24 +0500

    add hero-pill component, add flag to show promo on root page to pages app

--- apps/website/app/(root)/page.tsx
+++ apps/website/app/(root)/page.tsx
@@ -1,6 +1,9 @@
 import Dock from "@/components/dock/dock";
+import { HeroPill, HeroPillIcon, HeroPillText } from "@/components/hero-pill";
 import { Logo } from "@/components/logo";
 import PageDockFiller from "@/components/page-dock-filler";
+import { showPagesPromo } from "@/const/flags";
+import { ExternalLinkIcon, PackagePlusIcon } from "lucide-react";
 import Link from "next/link";
 import { cn } from "yz13/cn";
 import Footer from "./footer";
@@ -18,6 +21,25 @@ const page = async () => {
       <main className={cn("w-full max-w-xl space-y-0 mx-auto *:p-6 ")}>
         <Hero />
       </main>
+      {(await showPagesPromo()) && (
+        <div className="w-fit relative mx-auto flex items-center justify-center px-6 my-6">
+          <HeroPill>
+            <HeroPillIcon>
+              <PackagePlusIcon size={12} />
+            </HeroPillIcon>
+            <HeroPillText>
+              Pages - Библиотека страниц и компонентов
+            </HeroPillText>
+            <HeroPillIcon>
+              <ExternalLinkIcon size={12} />
+            </HeroPillIcon>
+          </HeroPill>
+          <Link
+            href="https://pages.yz13.ru"
+            className="absolute top-0 right-0 w-full h-full"
+          />
+        </div>
+      )}
       <div className="w-full max-w-xl mx-auto *:px-6 space-y-3">
         <RootGrid />
       </div>
