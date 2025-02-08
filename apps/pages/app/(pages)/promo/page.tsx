734bedfc33fa4f3b864acfa09af15ca3f91e63d0
YZ13-ENV <YZTHECEO@yandex.ru>
2025-02-08 18:59:46 +0500

    update promo page, add more images

--- apps/pages/app/(pages)/promo/page.tsx
+++ apps/pages/app/(pages)/promo/page.tsx
@@ -1,11 +1,9 @@
-import { Logo } from "@/components/logo";
 import { Typewriter } from "@/components/text-writter";
-import { ArrowRightIcon } from "lucide-react";
-import { Button } from "mono/components/button";
 import { Separator } from "mono/components/separator";
 import { Metadata } from "next";
-import Link from "next/link";
+import Image from "next/image";
 import config from "./page.config.json";
+import RecentBlog from "./recent-blog";
 import RecentProjects from "./recent-projects";
 
 export const metadata: Metadata = {
@@ -16,7 +14,7 @@ export const metadata: Metadata = {
 const page = async () => {
   return (
     <>
-      <div className="w-full h-16">
+      {/* <div className="w-full h-16">
         <header className="container mx-auto w-full h-full px-6 flex items-center justify-between">
           <Link href="/">
             <Logo size={{ width: 96, height: 18 }} type="full" />
@@ -26,8 +24,8 @@ const page = async () => {
             <ArrowRightIcon size={16} />
           </Button>
         </header>
-      </div>
-      <div className="container mx-auto w-full py-12 px-6">
+      </div> */}
+      <div className="container mx-auto w-full py-12 lg:!mt-24 mt-12 px-6">
         <Typewriter
           text={[
             "Фронтенд разработчик.",
@@ -50,56 +48,119 @@ const page = async () => {
         </div>
       )}
       <div className="container mx-auto w-full p-6">
-        <div className="w-full flex aspect-video border divide-x overflow-hidden rounded-xl bg-background-back">
-          <div className="w-1/4 h-full bg-background shadow-2xl">
+        <div className="w-full flex aspect-video border divide-x overflow-hidden rounded-3xl bg-background-back">
+          <div className="w-1/4 h-full overflow-hidden bg-background shadow-2xl hover:bg-background-back transition-colors">
             <div className="w-full gap-6 shrink-0 aspect-video flex justify-center items-center">
-              <div className="size-16 rounded-xl border" />
+              <div className="size-16 rounded-2xl bg-background border" />
               <Separator orientation="vertical" className="h-16" />
-              <span className="text-4xl font-medium">APP</span>
+              <span className="text-4xl font-medium">Reservia</span>
             </div>
             <div className="w-full h-full pl-6 py-6">
-              <div className="aspect-video h-full border rounded-xl"></div>
+              <div className="aspect-video h-full border rounded-xl">
+                <Image
+                  src="/pages/promo/reservia/map-editor-page.png"
+                  className="!static rounded-xl object-cover light-mode-image"
+                  fill
+                  alt="Reservia"
+                />
+                <Image
+                  src="/pages/promo/reservia/map-editor-page-dark.png"
+                  className="!static rounded-xl object-cover dark-mode-image"
+                  fill
+                  alt="Reservia"
+                />
+              </div>
             </div>
           </div>
-          <div className="w-1/4 h-full bg-background shadow-2xl">
+          <div className="w-1/4 h-full overflow-hidden bg-background shadow-2xl hover:bg-background-back transition-colors">
             <div className="w-full gap-6 shrink-0 aspect-video flex justify-center items-center">
-              <div className="size-16 rounded-xl border" />
+              <div className="size-16 rounded-2xl bg-background border" />
               <Separator orientation="vertical" className="h-16" />
-              <span className="text-4xl font-medium">APP</span>
+              <span className="text-4xl font-medium">YZ13</span>
             </div>
             <div className="w-full h-full pl-6 py-6">
-              <div className="aspect-[9/16] h-full border rounded-xl"></div>
+              <div className="w-[125%] border rounded-xl">
+                <Image
+                  src="/pages/promo/yz13/yz13-page.png"
+                  className="!static rounded-xl object-cover light-mode-image"
+                  fill
+                  alt="Reservia"
+                />
+                <Image
+                  src="/pages/promo/yz13/yz13-page-dark.png"
+                  className="!static rounded-xl object-cover dark-mode-image"
+                  fill
+                  alt="Reservia"
+                />
+              </div>
             </div>
           </div>
-          <div className="w-1/4 h-full bg-background shadow-2xl">
+          <div className="w-1/4 h-full overflow-hidden bg-background shadow-2xl hover:bg-background-back transition-colors">
             <div className="w-full gap-6 shrink-0 aspect-video flex justify-center items-center">
-              <div className="size-16 rounded-xl border" />
+              <div className="size-16 rounded-2xl bg-background border" />
               <Separator orientation="vertical" className="h-16" />
-              <span className="text-4xl font-medium">APP</span>
+              <span className="text-4xl font-medium">Maps</span>
             </div>
             <div className="w-full h-full pl-6 py-6">
-              <div className="aspect-video h-full border rounded-xl"></div>
+              <div className="aspect-video h-full border rounded-xl">
+                <Image
+                  src="/pages/promo/maps/map-project.png"
+                  className="!static rounded-xl object-cover light-mode-image"
+                  fill
+                  alt="Reservia"
+                />
+                <Image
+                  src="/pages/promo/maps/map-project-dark.png"
+                  className="!static rounded-xl object-cover dark-mode-image"
+                  fill
+                  alt="Reservia"
+                />
+              </div>
             </div>
           </div>
-          <div className="w-1/4 h-full bg-background shadow-2xl">
+          <div className="w-1/4 h-full overflow-hidden bg-background shadow-2xl hover:bg-background-back transition-colors">
             <div className="w-full gap-6 shrink-0 aspect-video flex justify-center items-center">
-              <div className="size-16 rounded-xl border" />
+              <div className="size-16 rounded-2xl bg-background border" />
               <Separator orientation="vertical" className="h-16" />
-              <span className="text-4xl font-medium">APP</span>
+              <span className="text-4xl font-medium">Drafts</span>
             </div>
             <div className="w-full h-full pl-6 py-6">
-              <div className="aspect-[9/16] h-full border rounded-xl"></div>
+              <div className="aspect-video h-full border rounded-xl">
+                <Image
+                  src="/pages/promo/drafts/drafts-project.png"
+                  className="!static rounded-xl object-cover light-mode-image"
+                  fill
+                  alt="Reservia"
+                />
+                <Image
+                  src="/pages/promo/drafts/drafts-project-dark.png"
+                  className="!static rounded-xl object-cover dark-mode-image"
+                  fill
+                  alt="Reservia"
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
       <div className="border-y">
-        <div className="container mx-auto w-full p-6">
+        <div className="container mx-auto w-full px-6 pb-6 pt-12">
           <h2 className="text-4xl font-medium">Последние проекты</h2>
         </div>
       </div>
       <div className="divide-y">
         <RecentProjects />
+      </div>
+      <div className="border-y">
+        <div className="container mx-auto w-full px-6 pb-6 pt-12">
+          <h2 className="text-4xl font-medium">Блог</h2>
+          <p className="text-4xl font-medium text-secondary">
+            Собственный журнал
+          </p>
+        </div>
+      </div>
+      <div className="divide-y">
+        <RecentBlog />
       </div>
     </>
   );
