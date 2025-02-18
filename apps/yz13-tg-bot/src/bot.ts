c6e93e06419a87cbec3b5969026ec3fe4fff961e
YZ13-ENV <YZTHECEO@yandex.ru>
2025-02-18 19:43:59 +0500

    fix tg-bot app

--- apps/yz13-tg-bot/src/bot.ts
+++ apps/yz13-tg-bot/src/bot.ts
@@ -1,9 +1,9 @@
 import { Bot } from "grammy";
 
-const { BOT_TOKEN: token = "" } = process.env;
+const BOT_TOKEN = process.env.BOT_TOKEN ?? "";
 
 // Set your token in the vercel environment variable
-export const bot = new Bot(token);
+export const bot = new Bot(BOT_TOKEN);
 
 // attach all middleware
 bot.on("message", async (ctx) => {
