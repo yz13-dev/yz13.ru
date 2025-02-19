9910f101658a43af00a8328d189a161df6c13c37
YZ13-ENV <YZTHECEO@yandex.ru>
2025-02-19 07:33:19 +0500

    update bot, replace tech icons

--- apps/yz13-tg-bot/src/bot.ts
+++ apps/yz13-tg-bot/src/bot.ts
@@ -1,4 +1,4 @@
-import { Bot } from "grammy";
+import { Bot, InlineKeyboard, InlineQueryResultBuilder } from "grammy";
 
 const BOT_TOKEN = process.env.BOT_TOKEN ?? "";
 
@@ -6,6 +6,27 @@ const BOT_TOKEN = process.env.BOT_TOKEN ?? "";
 export const bot = new Bot(BOT_TOKEN);
 
 // attach all middleware
-bot.on("message", async (ctx) => {
-  await ctx.reply("Hi there!");
+bot.on("message:text", async (ctx) => {
+  const { from } = ctx.message;
+  await ctx.reply(`Привет, ${from.first_name}!`);
+  const button = new InlineKeyboard().url("Открыть", "https://yz13.ru/");
+  await ctx.reply(
+    "Я сейчас нахожусь в тестовом режиме. Посетите сайт разработчика, возможно там найдется полезная информация.",
+    {
+      reply_markup: button,
+    },
+  );
+});
+
+// Return empty result list for other queries.
+bot.inlineQuery("website", async (ctx) => {
+  const result = InlineQueryResultBuilder.article("id:website", "Сайт", {
+    reply_markup: new InlineKeyboard().url("YZ13 Website", "https://yz13.ru/"),
+  }).text(`<b>YZ13</b> - Фронтенд разработчик`, { parse_mode: "HTML" });
+
+  // Answer the inline query.
+  await ctx.answerInlineQuery(
+    [result], // answer with result list
+    { cache_time: 24 * 3600 }, // 30 days in seconds
+  );
 });
