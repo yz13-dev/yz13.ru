9065566ef8b360b72aa93dc929493d76f456ddad
yz13-dev <yztheceo@yandex.ru>
2025-03-09 15:14:40 +0500

    update blog page, add dock

--- apps/website/app/blog/page.tsx
+++ apps/website/app/blog/page.tsx
@@ -1,37 +1,16 @@
-import Header from "@/components/header";
-import User from "@/components/user";
-import { showAppsLink } from "@/const/flags";
-import { LayoutGridIcon } from "lucide-react";
-import { Button } from "mono/components/button";
+import Dock from "@/components/dock/dock";
+import PageDockFiller from "@/components/page-dock-filler";
 import { SidebarProvider } from "mono/components/sidebar";
-import { Skeleton } from "mono/components/skeleton";
-import Link from "next/link";
-import { Suspense } from "react";
-import { isDev } from "../login/get-url";
 import AppSidebar from "./sidebar/app-sidebar";
 
 const page = async () => {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <div className="w-full">
-        <Header>
-          <div />
-          <div className="flex items-center gap-2">
-            <Suspense fallback={<Skeleton className="size-9" />}>
-              {(await showAppsLink()) && (
-                <Button variant="ghost" size="icon" asChild>
-                  <Link href="/apps">
-                    <LayoutGridIcon size={16} />
-                  </Link>
-                </Button>
-              )}
-            </Suspense>
-            <Suspense fallback={<Skeleton className="h-9 w-[75px]" />}>
-              {isDev && <User />}
-            </Suspense>
-          </div>
-        </Header>
+      <div className="w-full p-6">
+        <span className="text-base font-medium">Блог</span>
+        <PageDockFiller />
+        <Dock />
       </div>
     </SidebarProvider>
   );
