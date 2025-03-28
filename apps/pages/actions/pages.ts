ad093b0d1ba3190a7bbc20ebc5ec14e6b5c24b1c
yz13-dev <yztheceo@yandex.ru>
2025-03-28 21:16:13 +0500

    remove pages app

--- apps/pages/actions/pages.ts
+++
@@ -1,5 +0,0 @@
-import pagesJson from "@/pages.json";
-
-export const getPage = (id: string) => {
-  return pagesJson.find((page) => page.id === id);
-};
--- apps/pages/app/(root)/sidebar/app-sidebar.tsx
+++
@@ -1,7 +0,0 @@
-import { Sidebar } from "mono/components/sidebar";
-
-const AppSidebar = () => {
-  return <Sidebar></Sidebar>;
-};
-
-export default AppSidebar;
