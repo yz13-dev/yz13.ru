4ee093288e2192f80c273b09c3b1e54ecb4dd6e2
yz13-dev <yztheceo@yandex.ru>
2025-05-04 15:32:53 +0500

    add minimal output of events in day timeline on root page

--- apps/calendar/app/(root)/events/section.tsx
+++ apps/calendar/app/(root)/events/section.tsx
@@ -1,5 +1,5 @@
-import { format, parseISO } from "date-fns";
-import { Button } from "mono/components/button";
+import DayTimeline from "@/app/[date]/day-timeline";
+import { differenceInDays, format, parse } from "date-fns";
 import { Skeleton } from "mono/components/skeleton";
 import { getUserEvents } from "rest-api/calendar";
 
@@ -21,49 +21,34 @@ export default async function Section({
   uid,
   date = format(new Date(), "yyyy-MM-dd"),
 }: {
-  uid: string;
+  uid?: string;
   date?: string;
 }) {
+  const today = new Date();
+  const targetDate = parse(date, "yyyy-MM-dd", new Date());
+  const diffInDay = differenceInDays(targetDate, today);
+  if (!uid)
+    return (
+      <DayTimeline
+        dateRange={[diffInDay]}
+        timeRange={[0, 24]}
+        className="w-full"
+        timeline={{
+          showLabel: false,
+        }}
+      />
+    );
   const { data } = await getUserEvents(uid, { date });
   const events = data ?? [];
   return (
-    <>
-      <ul className="w-full h-fit space-y-3 py-3">
-        {events.slice(0, 9).map((event, i) => {
-          const summary = event.summary ?? "Нет названия";
-          const description = event.description ?? "Нет описания";
-          const parsed = parseISO(event.date_start);
-          const time = format(parsed, "HH:mm");
-          const isAllDay = event.all_day ?? false;
-          return (
-            <li className="w-full" key={i}>
-              <div className="w-full p-3 flex flex-row justify-between items-start rounded-xl hover:bg-background-secondary border transition-colors">
-                <div className="flex flex-col gap-1">
-                  <span className="text-base font-medium">{summary}</span>
-                  {description && (
-                    <span className="text-sm text-muted-foreground">
-                      {description}
-                    </span>
-                  )}
-                </div>
-                <span className="text-base font-medium">{time}</span>
-              </div>
-            </li>
-          );
-        })}
-        {events.length > 9 && (
-          <li className="w-full">
-            <Button
-              variant="ghost"
-              className="w-full h-10 flex items-center justify-center"
-            >
-              <span className="text-sm text-muted-foreground">
-                Показать еще
-              </span>
-            </Button>
-          </li>
-        )}
-      </ul>
-    </>
+    <DayTimeline
+      events={events}
+      dateRange={[diffInDay]}
+      timeRange={[0, 24]}
+      className="w-full"
+      timeline={{
+        showLabel: false,
+      }}
+    />
   );
 }
