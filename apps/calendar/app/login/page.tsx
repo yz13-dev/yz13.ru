f4b033877320e33125b3ac5ec4ff6716e2943fd3
yz13-dev <yztheceo@yandex.ru>
2025-05-07 17:37:30 +0500

    fix calendar login redirect

--- apps/calendar/app/login/page.tsx
+++ apps/calendar/app/login/page.tsx
@@ -2,7 +2,7 @@ import { appId } from "@/const/app-id";
 import { permanentRedirect } from "next/navigation";
 
 const page = () => {
-  const url = new URL("/login", "https://calendar.yz13.ru");
+  const url = new URL("/login", "https://yz13.ru");
   const searchParams = url.searchParams;
   if (appId) searchParams.set("appId", appId);
   return permanentRedirect(url.toString());
