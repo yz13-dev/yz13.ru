97f9a91e345fc6afb617a38fdaba254663a2d72f
yz13-dev <yztheceo@yandex.ru>
2025-05-17 18:53:24 +0500

    change root layout to more minimal

--- apps/website/app/(root)/components/stack.tsx
+++ apps/website/app/(root)/components/stack.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { cn } from "yz13/cn";
-
 const list = [
   "HTML",
   "CSS",
@@ -16,23 +14,7 @@ const list = [
 ];
 
 export default function Stack({ className = "" }: { className?: string }) {
-  return (
-    <div
-      className={cn(
-        "w-full flex items-start gap-2 flex-wrap *:bg-background",
-        className,
-      )}
-    >
-      {list.map((value, i) => {
-        return (
-          <span
-            key={value + i}
-            className="px-2 py-0.5 text-sm text-balance text-muted-foreground rounded-full border"
-          >
-            {value}
-          </span>
-        );
-      })}
-    </div>
-  );
+  return list.map((value, i) => {
+    return <span key={value + i}>{value}</span>;
+  });
 }
--- apps/website/app/(root)/page.tsx
+++ apps/website/app/(root)/page.tsx
@@ -2,7 +2,6 @@ import Availability, { AvailabilitySkeleton } from "@/components/availability";
 import { Logo } from "@/components/logo";
 import Footer from "@/components/small-footer";
 import { availableForWork } from "@/const/flags";
-import packageJson from "@/package.json";
 import { SendIcon } from "lucide-react";
 import { Button } from "mono/components/button";
 import Link from "next/link";
@@ -13,64 +12,44 @@ import CallToAction, {
 import OtherProjects, {
   OtherProjectsSkeleton,
 } from "./components/other-projects";
-import ServicesDetails from "./components/services-details";
-import Stack from "./components/stack";
 
 export default async function page() {
   const isAvailable = await availableForWork();
   const chat_url = "https://t.me/yz13_dev";
   return (
-    <>
-      <div className="max-w-6xl w-full mx-auto py-12 px-6 space-y-6">
-        <div className="size-24 relative border rounded-[25%] overflow-hidden shrink-0 flex items-center justify-center">
-          <Logo size={{ width: 64, height: 64 }} type="only-icon" />
-        </div>
-        <Suspense fallback={<AvailabilitySkeleton />}>
-          <Availability />
-        </Suspense>
-        <div className="flex md:w-2/3 w-full flex-col gap-6">
-          <main className="w-full space-x-2 *:font-semibold *:inline *:md:text-4xl *:text-3xl max-w-2xl">
-            <h1>YZ13</h1>
-            <span>—</span>
-            <p>
-              Фронтенд разработчик, специализируюсь на разработке сайтов,
-              веб-приложений.
-            </p>
-          </main>
-        </div>
-        <div className="flex items-center mt-6 gap-2">
-          <Suspense fallback={<CallToActionSkeleton />}>
-            <CallToAction busy={isAvailable} />
-          </Suspense>
-          <Button variant="ghost" asChild>
-            <Link href={chat_url}>
-              <SendIcon size={16} />
-              Чат
-            </Link>
-          </Button>
-        </div>
-      </div>
-      <div className="w-full gap-6 flex lg:flex-row flex-col max-w-6xl mx-auto p-6">
+    <div className="flex w-full lg:h-dvh h-fit items-center justify-center flex-col gap-12">
+      <div className="w-full lg:gap-6 gap-12 flex lg:flex-row flex-col max-w-6xl mx-auto p-6">
         <div className="lg:w-2/3 w-full space-y-8">
-          <div className="w-full space-y-4">
-            <span className="block text-lg font-medium">Услуги</span>
-            <div className="*:rounded-xl w-full *:border space-y-3 *:p-4">
-              <ServicesDetails />
-            </div>
+          <div className="size-24 relative border rounded-[25%] overflow-hidden shrink-0 flex items-center justify-center">
+            <Logo size={{ width: 64, height: 64 }} type="only-icon" />
           </div>
-          <div className="w-full space-y-4">
-            <span className="block text-lg font-medium">Стэк</span>
-            <Stack />
+          <Suspense fallback={<AvailabilitySkeleton />}>
+            <Availability />
+          </Suspense>
+          <div className="flex w-full flex-col gap-6">
+            <main className="w-full space-x-2 *:font-semibold *:inline *:md:text-4xl *:text-3xl">
+              <h1>YZ13</h1>
+              <span>—</span>
+              <p>
+                Фронтенд разработчик, специализируюсь на разработке сайтов,
+                веб-приложений.
+              </p>
+            </main>
           </div>
-          <div className="w-full space-y-4">
-            <span className="text-sm block font-medium">Последняя версия</span>
-            <span className="text-sm text-muted-foreground block">
-              {packageJson.version}
-            </span>
+          <div className="flex items-center mt-6 gap-2">
+            <Suspense fallback={<CallToActionSkeleton />}>
+              <CallToAction busy={isAvailable} />
+            </Suspense>
+            <Button variant="ghost" asChild>
+              <Link href={chat_url}>
+                <SendIcon size={16} />
+                Чат
+              </Link>
+            </Button>
           </div>
         </div>
-        <div className="lg:w-1/3 w-full space-y-6">
-          <div className="w-full space-y-3">
+        <div className="lg:w-1/3 w-full h-full flex items-end space-y-6">
+          <div className="w-full h-fit space-y-3">
             <span className="block text-lg font-medium">Другие проекты</span>
             <ul className="space-y-6">
               <Suspense fallback={<OtherProjectsSkeleton />}>
@@ -83,6 +62,6 @@ export default async function page() {
       <div className="w-full gap-6 max-w-6xl mx-auto p-6 space-y-6">
         <Footer />
       </div>
-    </>
+    </div>
   );
 }
