305cb6643ba5076f5ec40fe60c3ce63b2224008c
yz13-dev <yztheceo@yandex.ru>
2025-06-29 18:15:19 +0500

    clean up website app

--- apps/website/components/page-dock-filler.tsx
+++
@@ -1,7 +0,0 @@
-import { cn } from "@yz13/ui/cn";
-
-const PageDockFiller = ({ className = "" }: { className?: string }) => {
-  return <div className={cn("w-full h-24", className)} />;
-};
-
-export default PageDockFiller;
