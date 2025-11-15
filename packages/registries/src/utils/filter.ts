e4c2c4463a16c65f211ff0ee6f74e223c3eadfaa
yz13-dev <yztheceo@yandex.ru>
2025-11-15 12:17:58 +0500

    fix filter.ts in @yz13/registries

--- packages/registries/src/utils/filter.ts
+++ packages/registries/src/utils/filter.ts
@@ -1,35 +1,3 @@
-import type { Project } from "../projects";
-
-
-
-type Filter = {
-  [key in keyof Project]: Project[keyof Project];
-}
-
-export function filterProjects(projects?: Project[], filter?: Partial<Filter> | ((project: Project) => boolean)) {
-
-  if (!projects) return [];
-  if (!filter) return projects;
-
-  const filtered: Project[] = [];
-
-  for (const project of projects) {
-    let match = true;
-    for (const key in filter) {
-
-      const filterKey = key as keyof Filter;
-
-      if (project[filterKey] !== filter[filterKey]) {
-        match = false;
-        break;
-      }
-    }
-    if (match) filtered.push(project);
-  }
-
-  return filtered;
-
-}
 
 export function filter<T>(data?: T[], filter?: Partial<T> | ((item: T) => boolean)) {
 
