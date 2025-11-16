8da3d533a83e91e0653e812eb414cad463832824
yz13-dev <yztheceo@yandex.ru>
2025-11-16 12:59:11 +0500

    update supabase middlewares

--- apps/api/src/middlewares/supabase.middleware.ts
+++ apps/api/src/middlewares/supabase.middleware.ts
@@ -1,7 +1,6 @@
 import { createServerClient, parseCookieHeader } from '@supabase/ssr';
 import type { SupabaseClient } from '@supabase/supabase-js';
 import type { Context, MiddlewareHandler } from 'hono';
-import { env } from 'hono/adapter';
 import { setCookie } from 'hono/cookie';
 import type { CookieOptions } from 'hono/utils/cookie';
 
@@ -17,19 +16,10 @@ export const getSupabase = (c: Context) => {
   return c.get('supabase');
 };
 
-type SupabaseEnv = {
-  SUPABASE_URL: string;
-  SUPABASE_ANON_KEY: string;
-};
-
 export const supabaseMiddleware = (): MiddlewareHandler => {
   return async (c, next) => {
-    const supabaseEnv = env<SupabaseEnv>(c);
-    const supabaseUrl =
-      supabaseEnv.SUPABASE_URL || process.env.SUPABASE_URL;
-    const supabaseAnonKey =
-      supabaseEnv.SUPABASE_ANON_KEY ||
-      process.env.SUPABASE_ANON_KEY;
+    const supabaseUrl = process.env.SUPABASE_URL;
+    const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
     if (!supabaseUrl) {
       throw new Error('SUPABASE_URL missing!');
