ff7829896b5f073ebd041f967b94623cf1c91270
yz13-dev <yztheceo@yandex.ru>
2025-11-20 12:23:09 +0500

    add loading for news mini-app

---
+++ apps/website/app/(mini-apps)/news/loading.tsx
@@ -0,0 +1,31 @@
+import { HeaderSkeleton } from "@/components/header";
+import { Skeleton } from "@yz13/ui/skeleton";
+import { LastSkeleton } from "./components/last";
+import { RecentSkeleton } from "./components/recent";
+
+export default function Loading() {
+  return (
+    <>
+      <HeaderSkeleton />
+      <div className="container mx-auto px-6">
+        <div className="w-full md:pt-32 pt-24 md:pb-16 pb-12">
+          <Skeleton className="size-32 outline-6 border outline-border/40 transition-colors rounded-3xl bg-card" />
+        </div>
+      </div>
+      <div className="w-full container px-6 mx-auto h-fit py-6 space-y-12">
+        <div className="*:block max-w-4xl space-y-4">
+          <h1 className="text-6xl block font-medium">Новостная лента</h1>
+        </div>
+      </div>
+      <section className="space-y-4 container mx-auto p-6">
+        <RecentSkeleton />
+      </section>
+      <section className="container mx-auto px-6 pb-6">
+        <div className="py-6">
+          <h3 className="text-2xl font-medium">Последние новости</h3>
+        </div>
+        <LastSkeleton />
+      </section>
+    </>
+  )
+}
